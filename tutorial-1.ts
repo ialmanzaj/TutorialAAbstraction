cdd2e5fa6a8f7ab5ce07acb9a5871d2ef7bd1743
Isaac Almanza <isaac.almanza19@gmail.com>
2024-04-13 23:08:53 -0500

    feat: add safe tutorial

--- tutorial-1.ts
+++ tutorial-1.ts
@@ -1,68 +1,75 @@
-import "dotenv/config"
-import { writeFileSync } from "fs"
-import { ENTRYPOINT_ADDRESS_V07, createSmartAccountClient } from "permissionless"
-import { privateKeyToSimpleSmartAccount } from "permissionless/accounts"
+import "dotenv/config";
+import { writeFileSync } from "fs";
 import {
-	createPimlicoBundlerClient,
-	createPimlicoPaymasterClient,
-} from "permissionless/clients/pimlico"
-import { Hex, createPublicClient, http } from "viem"
-import { generatePrivateKey } from "viem/accounts"
-import { sepolia } from "viem/chains"
+  ENTRYPOINT_ADDRESS_V07,
+  createSmartAccountClient,
+} from "permissionless";
+import { privateKeyToSimpleSmartAccount } from "permissionless/accounts";
+import {
+  createPimlicoBundlerClient,
+  createPimlicoPaymasterClient,
+} from "permissionless/clients/pimlico";
+import { Hex, createPublicClient, http } from "viem";
+import { generatePrivateKey } from "viem/accounts";
+import { sepolia } from "viem/chains";
 
-const apiKey = process.env.PIMLICO_API_KEY
-if (!apiKey) throw new Error("Missing PIMLICO_API_KEY")
-const paymasterUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`
+const apiKey = process.env.PIMLICO_API_KEY;
+if (!apiKey) throw new Error("Missing PIMLICO_API_KEY");
+const paymasterUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`;
 
 const privateKey =
-	(process.env.PRIVATE_KEY as Hex) ??
-	(() => {
-		const pk = generatePrivateKey()
-		writeFileSync(".env", `PRIVATE_KEY=${pk}`)
-		return pk
-	})()
+  (process.env.PRIVATE_KEY as Hex) ??
+  (() => {
+    const pk = generatePrivateKey();
+    writeFileSync(".env", `PRIVATE_KEY=${pk}`);
+    return pk;
+  })();
 
 export const publicClient = createPublicClient({
-	transport: http("https://rpc.ankr.com/eth_sepolia"),
-})
+  transport: http("https://rpc.ankr.com/eth_sepolia"),
+});
 
 export const paymasterClient = createPimlicoPaymasterClient({
-	transport: http(paymasterUrl),
-	entryPoint: ENTRYPOINT_ADDRESS_V07,
-})
+  transport: http(paymasterUrl),
+  entryPoint: ENTRYPOINT_ADDRESS_V07,
+});
 
 const account = await privateKeyToSimpleSmartAccount(publicClient, {
-	privateKey,
-	entryPoint: ENTRYPOINT_ADDRESS_V07, // global entrypoint
-	factoryAddress: "0x91E60e0613810449d098b0b5Ec8b51A0FE8c8985",
-})
+  privateKey,
+  entryPoint: ENTRYPOINT_ADDRESS_V07, // global entrypoint
+  factoryAddress: "0x91E60e0613810449d098b0b5Ec8b51A0FE8c8985",
+});
 
-console.log(`Smart account address: https://sepolia.etherscan.io/address/${account.address}`)
+console.log(
+  `Smart account address: https://sepolia.etherscan.io/address/${account.address}`
+);
 
-const bundlerUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`
+const bundlerUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`;
 
 const bundlerClient = createPimlicoBundlerClient({
-	transport: http(bundlerUrl),
-	entryPoint: ENTRYPOINT_ADDRESS_V07,
-})
+  transport: http(bundlerUrl),
+  entryPoint: ENTRYPOINT_ADDRESS_V07,
+});
 
 const smartAccountClient = createSmartAccountClient({
-	account,
-	entryPoint: ENTRYPOINT_ADDRESS_V07,
-	chain: sepolia,
-	bundlerTransport: http(bundlerUrl),
-	middleware: {
-		gasPrice: async () => {
-			return (await bundlerClient.getUserOperationGasPrice()).fast
-		},
-		sponsorUserOperation: paymasterClient.sponsorUserOperation,
-	},
-})
+  account,
+  entryPoint: ENTRYPOINT_ADDRESS_V07,
+  chain: sepolia,
+  bundlerTransport: http(bundlerUrl),
+  middleware: {
+    gasPrice: async () => {
+      return (await bundlerClient.getUserOperationGasPrice()).fast;
+    },
+    sponsorUserOperation: paymasterClient.sponsorUserOperation,
+  },
+});
 
 const txHash = await smartAccountClient.sendTransaction({
-	to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
-	value: 0n,
-	data: "0x1234",
-})
+  to: "0x3bC25D139069Ca06f7079fE67dcEd166b40edA9e",
+  value: 0n,
+  data: "0x1234",
+});
 
-console.log(`User operation included: https://sepolia.etherscan.io/tx/${txHash}`)
+console.log(
+  `User operation included: https://sepolia.etherscan.io/tx/${txHash}`
+);
--- tutorial-3.ts
+++ tutorial-3.ts
@@ -1,102 +1,136 @@
-import "dotenv/config"
-import { writeFileSync } from "fs"
-import { ENTRYPOINT_ADDRESS_V07, createSmartAccountClient } from "permissionless"
-import { signerToSafeSmartAccount } from "permissionless/accounts"
-import { createPimlicoBundlerClient } from "permissionless/clients/pimlico"
-import { Hex, createPublicClient, encodeFunctionData, http, parseAbiItem } from "viem"
-import { generatePrivateKey, privateKeyToAccount } from "viem/accounts"
-import { sepolia } from "viem/chains"
+import "dotenv/config";
+import { writeFileSync } from "fs";
+import {
+  ENTRYPOINT_ADDRESS_V07,
+  createSmartAccountClient,
+} from "permissionless";
+import { signerToSafeSmartAccount } from "permissionless/accounts";
+import {
+  createPimlicoBundlerClient,
+  createPimlicoPaymasterClient,
+} from "permissionless/clients/pimlico";
+import {
+  Hex,
+  createPublicClient,
+  encodeFunctionData,
+  http,
+  parseAbiItem,
+} from "viem";
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { sepolia } from "viem/chains";
 
-const erc20PaymasterAddress = "0x000000000041F3aFe8892B48D88b6862efe0ec8d"
-const usdcAddress = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"
+export const buildTransaction = (
+  from: string,
+  recipient: string,
+  amount: number,
+  tokenAddress: string,
+  nonce: bigint,
+  chainId: number
+) => {
+  const transaction = {
+    nonce: Number(nonce),
+    value: 0n,
+    data: encodeFunctionData({
+      abi: [
+        parseAbiItem(
+          "function transfer(address recipient, uint256 amount) returns (bool)"
+        ),
+      ],
+      functionName: "transfer",
+      args: [recipient as Hex, BigInt(amount * 1e6)],
+    }),
+    from: from as Hex,
+    to: tokenAddress as Hex,
+    chainId: chainId,
+  };
+  return transaction;
+};
+
+const erc20PaymasterAddress = "0x000000000041F3aFe8892B48D88b6862efe0ec8d";
+const usdcAddress = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238";
 
 const privateKey =
-	(process.env.PRIVATE_KEY as Hex) ??
-	(() => {
-		const pk = generatePrivateKey()
-		writeFileSync(".env", `PRIVATE_KEY=${pk}`)
-		return pk
-	})()
+  (process.env.PRIVATE_KEY as Hex) ??
+  (() => {
+    const pk = generatePrivateKey();
+    writeFileSync(".env", `PRIVATE_KEY=${pk}`);
+    return pk;
+  })();
 
 const publicClient = createPublicClient({
-	transport: http("https://rpc.ankr.com/eth_sepolia"),
-})
+  transport: http("https://rpc.ankr.com/eth_sepolia"),
+});
 
-const apiKey = "YOUR_PIMLICO_API_KEY"
-const bundlerUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`
+const apiKey = process.env.PIMLICO_API_KEY;
+const bundlerUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${apiKey}`;
+
+export const paymasterClient = createPimlicoPaymasterClient({
+  transport: http(bundlerUrl),
+  entryPoint: ENTRYPOINT_ADDRESS_V07,
+});
 
 const bundlerClient = createPimlicoBundlerClient({
-	transport: http(bundlerUrl),
-	entryPoint: ENTRYPOINT_ADDRESS_V07,
-})
+  transport: http(bundlerUrl),
+  entryPoint: ENTRYPOINT_ADDRESS_V07,
+});
 
 const account = await signerToSafeSmartAccount(publicClient, {
-	signer: privateKeyToAccount(privateKey),
-	entryPoint: ENTRYPOINT_ADDRESS_V07, // global entrypoint
-	safeVersion: "1.4.1",
-	setupTransactions: [
-		{
-			to: usdcAddress,
-			value: 0n,
-			data: encodeFunctionData({
-				abi: [parseAbiItem("function approve(address spender, uint256 amount)")],
-				args: [
-					erc20PaymasterAddress,
-					0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffn,
-				],
-			}),
-		},
-	],
-})
+  signer: privateKeyToAccount(privateKey),
+  entryPoint: ENTRYPOINT_ADDRESS_V07, // global entrypoint
+  safeVersion: "1.4.1",
+});
 
-console.log(`Smart account address: https://sepolia.etherscan.io/address/${account.address}`)
+console.log(
+  `Smart account address: https://sepolia.etherscan.io/address/${account.address}`
+);
 
 const senderUsdcBalance = await publicClient.readContract({
-	abi: [parseAbiItem("function balanceOf(address account) returns (uint256)")],
-	address: usdcAddress,
-	functionName: "balanceOf",
-	args: [account.address],
-})
+  abi: [parseAbiItem("function balanceOf(address account) returns (uint256)")],
+  address: usdcAddress,
+  functionName: "balanceOf",
+  args: [account.address],
+});
+console.log("address", account.address);
 
 if (senderUsdcBalance < 1_000_000n) {
-	throw new Error(
-		`insufficient USDC balance for counterfactual wallet address ${account.address}: ${
-			Number(senderUsdcBalance) / 1000000
-		} USDC, required at least 1 USDC. Load up balance at https://faucet.circle.com/`,
-	)
+  throw new Error(
+    `insufficient USDC balance for counterfactual wallet address ${
+      account.address
+    }: ${
+      Number(senderUsdcBalance) / 1000000
+    } USDC, required at least 1 USDC. Load up balance at https://faucet.circle.com/`
+  );
 }
 
-console.log(`Smart account USDC balance: ${Number(senderUsdcBalance) / 1000000} USDC`)
+const txData = buildTransaction(
+  account.address,
+  "0x3bC25D139069Ca06f7079fE67dcEd166b40edA9e",
+  0.5,
+  usdcAddress,
+  0n,
+  sepolia.id
+);
+console.log("txData", txData);
+
+console.log(
+  `Smart account USDC balance: ${Number(senderUsdcBalance) / 1000000} USDC`
+);
 
 const smartAccountClient = createSmartAccountClient({
-	account,
-	entryPoint: ENTRYPOINT_ADDRESS_V07,
-	chain: sepolia,
-	bundlerTransport: http(bundlerUrl),
-	middleware: {
-		gasPrice: async () => {
-			return (await bundlerClient.getUserOperationGasPrice()).fast
-		},
-		sponsorUserOperation: async (args) => {
-			const gasEstimates = await bundlerClient.estimateUserOperationGas({
-				userOperation: {
-					...args.userOperation,
-					paymaster: erc20PaymasterAddress,
-				},
-			})
+  account,
+  entryPoint: ENTRYPOINT_ADDRESS_V07,
+  chain: sepolia,
+  bundlerTransport: http(bundlerUrl),
+  middleware: {
+    gasPrice: async () => {
+      return (await bundlerClient.getUserOperationGasPrice()).fast;
+    },
+    sponsorUserOperation: paymasterClient.sponsorUserOperation,
+  },
+});
 
-			return {
-				...gasEstimates,
-				paymaster: erc20PaymasterAddress,
-			}
-		},
-	},
-})
+const txHash = await smartAccountClient.sendTransaction(txData);
 
-const txHash = await smartAccountClient.sendTransaction({
-	to: "0xd8da6bf26964af9d7eed9e03e53415d37aa96045",
-	value: 0n,
-	data: "0x1234",
-})
-
-console.log(`User operation included: https://sepolia.etherscan.io/tx/${txHash}`)
+console.log(
+  `User operation included: https://sepolia.etherscan.io/tx/${txHash}`
+);
